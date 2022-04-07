5c276f9e5947828d2691be724d28a92fcb86774e
Amit <ak672676@gmail.com>
2022-04-07 14:13:47 +0530

    test complete

--- pages/pools/new.js
+++ pages/pools/new.js
@@ -1,32 +1,56 @@
 import react from "react";
 import Layout from "../../components/Layout";
+
 import factory from "../../ethereum/factory";
 import web3 from "../../ethereum/web3";
+
 import { Router } from "../../routes";
-import { Editor, EditorState } from "draft-js";
+
+import { EditorState, convertToRaw, convertFromRaw } from "draft-js";
+import draftToHtml from "draftjs-to-html";
+import dynamic from "next/dynamic";
+const Editor = dynamic(
+  () => import("react-draft-wysiwyg").then((mod) => mod.Editor),
+  { ssr: false }
+);
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
 class PoolNew extends react.Component {
-  state = {
-    minimumContribution: "",
-    title: "",
-    description: "",
-    errorMessage: "",
-    loading: false,
+  constructor(props) {
+    super(props);
+    this.state = {
+      minimumContribution: "",
+      title: "",
+      description: "",
+      errorMessage: "",
+      loading: false,
+      previewTab: false,
+      editorState: EditorState.createEmpty(),
+      editorState2: EditorState.createEmpty(),
+      message: "Lets try",
+    };
+    this.onEditorStateChange = this.onEditorStateChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onEditorStateChange = (editorState) => {
+    this.setState({
+      editorState,
+    });
   };
 
   onSubmit = async (event) => {
     event.preventDefault();
-    console.log(this.state);
+    var saved = JSON.stringify(
+      convertToRaw(this.state.editorState.getCurrentContent())
+    );
+
     this.setState({ loading: true, errorMessage: "" });
 
     try {
       const accounts = await web3.eth.getAccounts();
       await factory.methods
-        .createPool(
-          this.state.minimumContribution,
-          this.state.title,
-          this.state.description
-        )
+        .createPool(this.state.minimumContribution, this.state.title, saved)
         .send({
           from: accounts[0],
         });
@@ -39,91 +63,150 @@ class PoolNew extends react.Component {
   };
 
   render() {
+    var previewTab = this.state.previewTab;
+    var color = "indigo";
     return (
       <Layout>
-        <div className="w-full  h-screen flex justify-center items-center">
-          <div className=" bg-white w-1/2 m-h-2/3  mt-[-100px] shadow-lg rounded-md p-8">
-            <form onSubmit={this.onSubmit}>
-              <div className="mb-2">
-                <label
-                  htmlFor="title"
-                  className="block mb-2 text-sm font-medium text-gray-900"
-                >
-                  Title
-                </label>
-                <input
-                  type="text"
-                  id="title"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
-                  placeholder="Title"
-                  required
-                  value={this.state.title}
-                  onChange={(event) =>
-                    this.setState({ title: event.target.value })
-                  }
-                />
-              </div>
-              <div className="mb-2">
-                <label
-                  htmlFor="description"
-                  className="block mb-2 text-sm font-medium text-gray-900 "
-                >
-                  Your message
-                </label>
-                <textarea
-                  id="description"
-                  rows="4"
-                  className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 "
-                  placeholder="Description"
-                  value={this.state.description}
-                  onChange={(event) =>
-                    this.setState({ description: event.target.value })
-                  }
-                ></textarea>
-              </div>
-              <div className="mb-2">
-                <label
-                  htmlFor="minContribution"
-                  className="block mb-2 text-sm font-medium text-gray-900"
-                >
-                  Minimum contribution
-                </label>
-                <input
-                  type="number"
-                  id="minContribution"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
-                  placeholder="10000"
-                  value={this.state.minimumContribution}
-                  onChange={(event) =>
-                    this.setState({ minimumContribution: event.target.value })
-                  }
-                  required
-                />
-              </div>
-              {!!this.state.errorMessage && (
-                <div
-                  className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mb-2"
-                  role="alert"
-                >
-                  <p>{this.state.errorMessage}</p>
+        <div className="w-full  min-h-screen bg-slate-200 px-16">
+          <h1 className="text-slate-700 text-3xl font-semibold uppercase text-center pt-4">
+            Creating a new pool
+          </h1>
+          {!!this.state.errorMessage && (
+            <div
+              className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mb-2"
+              role="alert"
+            >
+              <p>{this.state.errorMessage}</p>
+            </div>
+          )}
+          <div className="my-4">
+            <input
+              type="text"
+              id="large-input"
+              placeholder="Title"
+              value={this.state.title}
+              onChange={(event) => this.setState({ title: event.target.value })}
+              maxLength={35}
+              className="block p-4 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-md focus:ring-indigo-300 focus:border-indigo-300  "
+            />
+          </div>
+          <div className="my-4">
+            <input
+              value={this.state.minimumContribution}
+              onChange={(event) =>
+                this.setState({ minimumContribution: event.target.value })
+              }
+              type="number"
+              id="large-input"
+              placeholder="Minimum Contribution (Wei)"
+              className="block p-4 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-md focus:ring-indigo-300 focus:border-indigo-300  "
+            />
+          </div>
+          <div className="flex flex-wrap">
+            <div className="w-full">
+              <ul
+                className="flex mb-0 list-none flex-wrap pt-3 pb-4 flex-row"
+                role="tablist"
+              >
+                <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
+                  <a
+                    className={
+                      "text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal " +
+                      (!previewTab
+                        ? "text-white bg-" + color + "-400"
+                        : "text-" + color + "-600 bg-white")
+                    }
+                    onClick={(e) => {
+                      e.preventDefault();
+                      this.setState({
+                        previewTab: !previewTab,
+                      });
+                    }}
+                    data-toggle="tab"
+                    href="#link1"
+                    role="tablist"
+                  >
+                    Insert Description
+                  </a>
+                </li>
+                <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
+                  <a
+                    className={
+                      "text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal " +
+                      (previewTab
+                        ? "text-white bg-" + color + "-400"
+                        : "text-" + color + "-600 bg-white")
+                    }
+                    onClick={(e) => {
+                      e.preventDefault();
+                      this.setState({
+                        message: draftToHtml(
+                          convertToRaw(
+                            this.state.editorState.getCurrentContent()
+                          )
+                        ),
+                      });
+                      this.setState({
+                        previewTab: !previewTab,
+                      });
+                    }}
+                    data-toggle="tab"
+                    href="#link2"
+                    role="tablist"
+                  >
+                    Preview
+                  </a>
+                </li>
+              </ul>
+              <div className="relative flex flex-col min-w-0 break-words bg-white w-full min-h-screen mb-6 shadow-lg rounded">
+                <div className="px-4 py-5 flex-auto">
+                  <div className="tab-content tab-space">
+                    <div
+                      className={!previewTab ? "block" : "hidden"}
+                      id="link1"
+                    >
+                      <Editor
+                        toolbarClassName="toolbarClassName"
+                        wrapperClassName="wrapperClassName"
+                        editorClassName="editorClassName"
+                        wrapperStyle={{
+                          width: "100p%",
+                          height: "100%",
+                          border: "1px solid grey",
+                        }}
+                        editorState={this.state.editorState}
+                        onEditorStateChange={this.onEditorStateChange}
+                      />
+                    </div>
+                    <div className={previewTab ? "block" : "hidden"} id="link2">
+                      <div
+                        className="unreset h-full w-full"
+                        dangerouslySetInnerHTML={{ __html: this.state.message }}
+                      ></div>
+                    </div>
+                  </div>
                 </div>
-              )}
-              {this.state.loading ? (
-                <button
-                  type="submit"
-                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center "
-                >
-                  Loading
-                </button>
-              ) : (
-                <button
-                  type="submit"
-                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center "
-                >
-                  Submit
-                </button>
-              )}
-            </form>
+              </div>
+            </div>
+          </div>
+          <div className="my-2">
+            {this.state.loading ? (
+              <button
+                disabled
+                type="submit"
+                className="text-white mb-4  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center "
+              >
+                Loading
+              </button>
+            ) : (
+              <button
+                onClick={this.onSubmit}
+                type="submit"
+                className="text-white mb-4  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center "
+              >
+                Submit
+              </button>
+            )}
           </div>
         </div>
       </Layout>
--- test/Pool.test.js
+++ test/Pool.test.js
@@ -1,7 +1,8 @@
 const assert = require("assert");
 const ganache = require("ganache-cli");
+const options = { gasLimit: 8000000 };
 const Web3 = require("web3");
-const web3 = new Web3(ganache.provider());
+const web3 = new Web3(ganache.provider(options));
 
 const compiledFactory = require("../ethereum/build/PoolFactory.json");
 const compiledPool = require("../ethereum/build/Pool.json");
@@ -16,11 +17,11 @@ beforeEach(async () => {
 
   factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
     .deploy({ data: compiledFactory.bytecode })
-    .send({ from: accounts[0], gas: "1000000" });
+    .send({ gas: "8000000", from: accounts[0] });
 
   await factory.methods.createPool("100", "TITLE", "DESC").send({
     from: accounts[0],
-    gas: "1000000",
+    gas: "8000000",
   });
 
   [poolAddress] = await factory.methods.getdeployedPools().call();
@@ -35,4 +36,70 @@ describe("Pool Test", () => {
     assert.ok(factory.options.address);
     assert.ok(pool.options.address);
   });
+
+  it("make caller pool owner", async () => {
+    const owner = await pool.methods.owner().call();
+    assert.equal(accounts[0], owner);
+  });
+
+  it("allows people to contribute to the pool and mark them as approver", async () => {
+    await pool.methods.contribute().send({
+      value: "200",
+      from: accounts[1],
+    });
+    const isContributor = await pool.methods.approvers(accounts[1]).call();
+    assert(isContributor);
+  });
+
+  it("check minimum contribution", async () => {
+    try {
+      await pool.methods.contribute().send({
+        value: "5",
+        from: accounts[1],
+      });
+      assert(false);
+    } catch (err) {
+      assert(err);
+    }
+  });
+
+  it("allows owner to make a payment request", async () => {
+    await pool.methods
+      .createRequest("Buy hardware", "100", accounts[1])
+      .send({
+        from: accounts[0],
+        gas: "1000000",
+      });
+    const request = await pool.methods.requests(0).call();
+
+    assert.equal("Buy hardware", request.description);
+  });
+
+  it("payment request process", async () => {
+    await pool.methods.contribute().send({
+      from: accounts[0],
+      value: web3.utils.toWei("10", "ether"),
+    });
+
+    await pool.methods
+      .createRequest("A", web3.utils.toWei("5", "ether"), accounts[1])
+      .send({ from: accounts[0], gas: "1000000" });
+
+    await pool.methods.approveRequest(0).send({
+      from: accounts[0],
+      gas: "1000000",
+    });
+
+    await pool.methods.finalizeRequest(0).send({
+      from: accounts[0],
+      gas: "1000000",
+    });
+
+    let balance = await web3.eth.getBalance(accounts[1]);
+    balance = web3.utils.fromWei(balance, "ether");
+    balance = parseFloat(balance);
+    console.log(balance);
+    assert(balance > 104);
+  });
+
 });
